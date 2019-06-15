29c809bf647e16732d2a00adda326535fbb79059
Vigneshwaran V <43519088+Vigneshwaran07@users.noreply.github.com>
2019-06-15 11:05:42 +0530

    Create five and eight.py

---
+++ five and eight.py
@@ -0,0 +1,16 @@
+a = input().replace(',','')
+t = 0
+s,e = 0,0
+f1,f2 = 0,0
+for ind,i in enumerate(a):
+    if(i=='5' and f1==0 and f2==0):
+        f1 = 1
+        f2 = 1
+        s = ind
+    elif(i=='8' and f1==1):
+        t += int(a[s:ind+1])
+        f1 = 0
+        f2 = 0
+    elif(f1==0 and f2==0):
+        t += int(i)
+print(t)
